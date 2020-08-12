79d27ae596955f286e39fa7c8b7770e7efeaf271
twinh <twinhuang@qq.com>
2020-08-12 11:08:59 +0800

    feat: add babel-preset-miaoxing

---
+++ index.js
@@ -0,0 +1,52 @@
+module.exports = function() {
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          "modules": false,
+          "useBuiltIns": "entry",
+          "corejs": {
+            "version": 3
+          }
+        }
+      ],
+      "@babel/preset-react",
+      "@emotion/babel-preset-css-prop",
+    ],
+    plugins: [
+      // NOTE: preset 里是反序
+      // NOTE: import 需在 auto-import-name 之前才正常加载
+      "auto-import-name",
+
+      // Stage 1
+      "@babel/plugin-proposal-export-default-from",
+      "@babel/plugin-proposal-logical-assignment-operators",
+      "@babel/plugin-proposal-optional-chaining",
+
+      // Stage 2
+      ["@babel/plugin-proposal-decorators", {"legacy": true}],
+      "@babel/plugin-proposal-export-namespace-from",
+
+      // Stage 3
+      "@babel/plugin-syntax-dynamic-import",
+      ["@babel/plugin-proposal-class-properties", {"loose": true}],
+
+      // Stage 4
+      "@babel/plugin-proposal-object-rest-spread",
+
+      // Others
+      "react-hot-loader/babel",
+      ["import", {
+        "libraryName": "antd",
+        "libraryDirectory": "es",
+        "style": true,
+      }],
+      ["import", {
+        "libraryName": "@mxjs/bootstrap",
+        "libraryDirectory": "",
+        "camel2DashComponentName": false,
+      }, '@mxjs/bootstrap'],
+    ]
+  };
+}
