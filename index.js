820928325c4a1092448eedbbf985dd8fa5e49c88
twinh <twinhuang@qq.com>
2020-08-14 17:12:33 +0800

    style(eslint): single quotes, double jsx-quotes

--- index.js
+++ index.js
@@ -2,49 +2,49 @@ module.exports = function() {
   return {
     presets: [
       [
-        "@babel/preset-env",
+        '@babel/preset-env',
         {
-          "useBuiltIns": "entry",
-          "corejs": {
-            "version": 3
+          'useBuiltIns': 'entry',
+          'corejs': {
+            'version': 3
           }
         }
       ],
-      "@babel/preset-react",
-      "@emotion/babel-preset-css-prop",
+      '@babel/preset-react',
+      '@emotion/babel-preset-css-prop',
     ],
     plugins: [
       // NOTE: preset 里是反序
       // NOTE: import 需在 auto-import-name 之前才正常加载
-      "auto-import-name",
+      'auto-import-name',
 
       // Stage 1
-      "@babel/plugin-proposal-export-default-from",
-      "@babel/plugin-proposal-logical-assignment-operators",
-      "@babel/plugin-proposal-optional-chaining",
+      '@babel/plugin-proposal-export-default-from',
+      '@babel/plugin-proposal-logical-assignment-operators',
+      '@babel/plugin-proposal-optional-chaining',
 
       // Stage 2
-      ["@babel/plugin-proposal-decorators", {"legacy": true}],
-      "@babel/plugin-proposal-export-namespace-from",
+      ['@babel/plugin-proposal-decorators', {'legacy': true}],
+      '@babel/plugin-proposal-export-namespace-from',
 
       // Stage 3
-      "@babel/plugin-syntax-dynamic-import",
-      ["@babel/plugin-proposal-class-properties", {"loose": true}],
+      '@babel/plugin-syntax-dynamic-import',
+      ['@babel/plugin-proposal-class-properties', {'loose': true}],
 
       // Stage 4
-      "@babel/plugin-proposal-object-rest-spread",
+      '@babel/plugin-proposal-object-rest-spread',
 
       // Others
-      "react-hot-loader/babel",
-      ["import", {
-        "libraryName": "antd",
-        "libraryDirectory": "es",
-        "style": true,
+      'react-hot-loader/babel',
+      ['import', {
+        'libraryName': 'antd',
+        'libraryDirectory': 'es',
+        'style': true,
       }],
-      ["import", {
-        "libraryName": "@mxjs/bootstrap",
-        "libraryDirectory": "",
-        "camel2DashComponentName": false,
+      ['import', {
+        'libraryName': '@mxjs/bootstrap',
+        'libraryDirectory': '',
+        'camel2DashComponentName': false,
       }, '@mxjs/bootstrap'],
     ]
   };
