74140f38729f8dd2647db6fe38c550171c49c5bc
twinh <twinhuang@qq.com>
2021-04-30 00:28:22 +0800

    refactor: 更新 babel 版本和 stage 信息，移除已包含在 `@babel/preset-env` 中的包

--- index.js
+++ index.js
@@ -28,18 +28,18 @@ module.exports = function () {
 
       // Stage 1
       '@babel/plugin-proposal-export-default-from',
-      '@babel/plugin-proposal-logical-assignment-operators',
-      '@babel/plugin-proposal-optional-chaining',
 
       // Stage 2
       ['@babel/plugin-proposal-decorators', {'legacy': true}],
-      '@babel/plugin-proposal-export-namespace-from',
 
       // Stage 3
-      '@babel/plugin-syntax-dynamic-import',
-      ['@babel/plugin-proposal-class-properties', {'loose': true}],
+      '@babel/plugin-proposal-class-properties',
 
       // Stage 4
+      '@babel/plugin-proposal-logical-assignment-operators',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-proposal-export-namespace-from',
       '@babel/plugin-proposal-object-rest-spread',
 
       // Others
